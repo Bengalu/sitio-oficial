a521318491dfa64691c52014a5e2804af238d1f8
benjamin <benjamin.official.2019@gmail.com>
2024-01-16 15:27:52 -0300

    fix tittle

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Bengalug</title>
+        <title>Bengalu</title>
         <link rel="shortcut icon" href="svg/bengalu.svg" />
       </head>
       <body >
